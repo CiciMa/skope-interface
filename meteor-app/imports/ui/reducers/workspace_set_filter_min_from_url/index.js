fddff1874b4e42a02f8b2e468f813c0a12db63fa
Piotrek605 <piotrw605@gmail.com>
2017-05-27 23:21:06 -0500

    debug min

--- meteor-app/imports/ui/reducers/workspace_set_filter_min_from_url/index.js
+++ meteor-app/imports/ui/reducers/workspace_set_filter_min_from_url/index.js
@@ -1,8 +1,6 @@
 /**
- * This reducer is used when filter value is changed.
+ * This reducer is used when filter min value is changed.
  */
-
-import { filterMax } from "/imports/ui/consts";
 
 export const WORKSPACE_SET_FILTER_MIN_FROM_URL = (state, action) => {
   const {
