e82db7874a1aca87838919a6245d91bd314a92fc
Piotrek605 <piotrw605@gmail.com>
2017-06-07 09:51:46 -0500

    reducers index bug

--- meteor-app/imports/ui/actions/workspace_set_filter_min_from_url/index.js
+++ meteor-app/imports/ui/actions/workspace_set_filter_min_from_url/index.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-export const WORKSPACE_SET_FILTER_FROM_URL = {
+export const WORKSPACE_SET_FILTER_MIN_FROM_URL = {
   type: "WORKSPACE_SET_FILTER_MIN_FROM_URL",
   payloadSchema: {
     value: PropTypes.string,
