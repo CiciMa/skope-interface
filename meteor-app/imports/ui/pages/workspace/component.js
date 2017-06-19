4a58588e45aec1b513bc6704cc8be2e30ea6e4c9
Piotrek605 <piotrw605@gmail.com>
2017-06-19 17:12:56 -0500

    welcome message

--- meteor-app/imports/ui/pages/workspace/component.js
+++ meteor-app/imports/ui/pages/workspace/component.js
@@ -219,12 +219,15 @@ export default class Page_Workspace extends React.Component {
 
     return (
       <div className="page--workspace">
+        
         <div className="welcome_frame">
-            <div className="welcome_info">
-                <h3>Model Run Metadata</h3>
-                <p>This is the metadata of the layers.</p>
-            </div>
         </div>
+        
+        <div className="welcome_info">
+            <h3>Model Run Metadata</h3>
+            <p>This is the metadata of the layers.</p>
+        </div>
+        
         <fieldset>
           <legend>Filters</legend>
           <div className="section_filter">
