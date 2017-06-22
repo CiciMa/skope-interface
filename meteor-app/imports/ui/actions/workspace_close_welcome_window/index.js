7cc3053b8d0ea1008f2d2c13d7c7a4b1b15e2273
Piotrek605 <piotrw605@gmail.com>
2017-06-22 14:24:43 -0500

    close window

--- meteor-app/imports/ui/actions/workspace_close_welcome_window/index.js
+++ meteor-app/imports/ui/actions/workspace_close_welcome_window/index.js
@@ -3,6 +3,6 @@ import PropTypes from "prop-types";
 export const WORKSPACE_CLOSE_WELCOME_WINDOW = {
   type: "WORKSPACE_CLOSE_WELCOME_WINDOW",
   payloadSchema: {
-    welcomeWindowClosed: PropTypes.bool.isRequired;
+    welcomeWindowClosed: PropTypes.bool.isRequired,
   },
 };
