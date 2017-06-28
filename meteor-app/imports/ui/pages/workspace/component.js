5e36429ae8f2b0e9d5cfae8d5ca6f2c2839784d8
Piotrek605 <piotrw605@gmail.com>
2017-06-28 16:13:00 -0500

    layout

--- meteor-app/imports/ui/pages/workspace/component.js
+++ meteor-app/imports/ui/pages/workspace/component.js
@@ -330,7 +330,7 @@ export default class Page_Workspace extends React.Component {
                 </map-view>
               </div>
 
-          <div>
+          <div className="section_data">
             {
               !inspectPointSelected
               ? <span>Choose a point on the map to view the corresponding charts</span>
@@ -342,7 +342,7 @@ export default class Page_Workspace extends React.Component {
                       </div>
                     )
                   : (
-                      <div className="section_data">
+                      <div>
                         <div className="section_range">
                             <div className="filter-min">
                               <label>Start: </label>
