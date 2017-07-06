adaad7c81040722d44df96f161c26be455408f98
Piotrek605 <piotrw605@gmail.com>
2017-07-06 16:41:39 -0500

    layout

--- meteor-app/imports/ui/layouts/full-window/component.js
+++ meteor-app/imports/ui/layouts/full-window/component.js
@@ -14,14 +14,8 @@ export default class Layout_FullWindow extends React.Component {
     } = this.props;
     return (
       <div className="page layout-fullWindow">
-        <div className="page__header">
-          <Navbar store={store} />
-        </div>
         <div className="page__body">
           {body}
-        </div>
-        <div className="page__footer">
-          <a href={demo_repository} target="_blank">Source code on Github</a>
         </div>
       </div>
     );
